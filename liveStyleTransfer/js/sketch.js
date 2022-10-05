4afca07d5f78918d53506c311f455bae02b6d6ca
M Shams Ul Haq Ghani <100300419+shamsghani@users.noreply.github.com>
2022-10-05 23:12:14 +0500

    Add files via upload

---
+++ liveStyleTransfer/js/sketch.js
@@ -0,0 +1,35 @@
+
+let style;
+let video;
+let resultImg;
+
+function setup() {
+  createCanvas(320, 240).parent('canvasContainer');
+
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // The results image from the style transfer
+  resultImg = createImg('');
+  resultImg.hide();
+
+  // Create a new Style Transfer method with a defined style.
+  // We give the video as the second argument
+  style = ml5.styleTransfer('models/acid', video, modelLoaded);
+}
+
+function draw(){
+  image(resultImg, 0, 0, 320, 240);
+}
+
+// A function to call when the model has been loaded.
+function modelLoaded() {
+  select('#status').html('Model Loaded');
+  style.transfer(gotResult);
+}
+
+// When we get the results, update the result image src
+function gotResult(err, img) {
+  resultImg.attribute('src', img.src);
+  style.transfer(gotResult);
+}
